b20509db373ce01714920a2c37dc6bef7c796aed
ahmedmorra100-code <ahmedmorra100@gmail.com>
2026-08-30 12:55:12 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 set -e
 
 VNC_PASS="${VNC_PASSWORD:-12345678}"
-mkdir -p /run /home/user/.vnc
+mkdir -p /run /root/.vnc
 x11vnc -storepasswd "$VNC_PASS" /run/vnc.pass >/dev/null 2>&1 || true
 chmod 600 /run/vnc.pass || true
 
@@ -10,11 +10,11 @@ echo "Starting Xvfb..."
 Xvfb :1 -screen 0 ${XRES:-1280x800x24} &
 sleep 1
 
-echo "Starting XFCE Desktop..."
+echo "Starting XFCE Desktop as ROOT..."
 export DISPLAY=:1
-export USER=user
-export HOME=/home/user
-su - user -c "DISPLAY=:1 startxfce4" &
+export USER=root
+export HOME=/root
+DISPLAY=:1 startxfce4 &
 sleep 2
 
 echo "Starting x11vnc..."
